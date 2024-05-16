37bcfce9bab7925e527cb7267453d10e38ad2a27
parham-k <pkazemi3@gmail.com>
2024-05-16 14:22:33 -0700

    Finish query subcommand

--- src/subcommands/query.hpp
+++ src/subcommands/query.hpp
@@ -20,6 +20,7 @@ get_argument_parser()
   parser->add_argument("-s").help("path to spaced seeds file (one per line)");
   parser->add_argument("-t").help("number of threads").default_value(1U).scan<'u', unsigned>();
   parser->add_argument("-b").help("path to BF/CBF file").required();
+  parser->add_argument("-o").help("path to output TSV file").required();
   parser->add_argument("--distinct").default_value(false).implicit_value(true);
   parser->add_argument("query").help("path to query data").required().remaining();
   return parser;
@@ -29,47 +30,62 @@ template<class BloomFilter, class HashFunction>
 inline void
 query(const BloomFilter& bf,
       HashFunction& hash_fn,
-      const std::vector<std::string>& query_path,
-      bool distinct)
+      const std::vector<std::string>& query_files,
+      bool distinct,
+      const std::string& out_path)
 {
+  Timer timer;
+  timer.start("performing queries");
   btllib::BloomFilter all(distinct ? bf.get_bytes() : 1, bf.get_hash_num());
-  std::cout << std::boolalpha;
-  for (const auto& file_path : query_path) {
-    btllib::SeqReader seq_reader(query_path, btllib::SeqReader::Flag::SHORT_MODE);
+  std::ofstream out_file(out_path);
+  out_file << std::boolalpha;
+  for (const auto& file : query_files) {
+    btllib::SeqReader seq_reader(file, btllib::SeqReader::Flag::SHORT_MODE);
 #pragma omp parallel shared(seq_reader)
     for (const auto& record : seq_reader) {
       hash_fn.set_seq(record.seq);
       while (hash_fn.roll()) {
-        if (!distinct || !all.contains_insert(hash)) {
+        if (!distinct || !all.contains_insert(hash_fn.hashes())) {
           const auto kmer = record.seq.substr(hash_fn.get_pos(), hash_fn.get_k());
-          std::cout << kmer << "\t" << bf.contains(hash_fn.get_hashes()) << std::endl;
+#pragma omp critical
+          out_file << kmer << "\t" << (unsigned)bf.contains(hash_fn.hashes()) << std::endl;
         }
       }
     }
   }
+  timer.stop();
 }
 
 int
 main(const argparse::ArgumentParser& args)
 {
   const auto num_threads = args.get<unsigned>("-t");
-  const auto query_data = args.get<std::vector<std::string>>("query");
-  const auto bf_path = args.get("-b");
-  const auto distinct = args.get<bool>("--distinct");
   omp_set_num_threads(num_threads);
   std::cout << "[-t] thread limit set to " << num_threads << std::endl;
+  const auto bf_path = args.get("-b");
+  const auto distinct = args.get<bool>("--distinct");
+  const auto files = args.get<std::vector<std::string>>("query");
+  const auto out_path = args.get("-o");
+  Timer timer;
   if (btllib::KmerBloomFilter::is_bloom_file(bf_path)) {
+    timer.start("[-b] loading k-mer bloom filter from " + bf_path);
     btllib::KmerBloomFilter bf(bf_path);
-    btllib::NtHash hash_fn("", bf.get_hash_num(), bf.get_k());
-    query<btllib::KmerBloomFilter, btllib::NtHash>(bf, hash_fn, query_data, distinct);
+    timer.stop();
+    btllib::NtHash hash_fn(std::string(bf.get_k(), 'N'), bf.get_hash_num(), bf.get_k());
+    query<btllib::KmerBloomFilter, btllib::NtHash>(bf, hash_fn, files, distinct, out_path);
   } else if (btllib::KmerCountingBloomFilter8::is_bloom_file(bf_path)) {
+    timer.start("[-b] loading k-mer counting bloom filter from " + bf_path);
     btllib::KmerCountingBloomFilter8 bf(bf_path);
-    btllib::NtHash hash_fn("", bf.get_hash_num(), bf.get_k());
-    query<btllib::KmerCountingBloomFilter8, btllib::NtHash>(bf, hash_fn, query_data, distinct);
+    timer.stop();
+    btllib::NtHash hash_fn(std::string(bf.get_k(), 'N'), bf.get_hash_num(), bf.get_k());
+    query<btllib::KmerCountingBloomFilter8, btllib::NtHash>(bf, hash_fn, files, distinct, out_path);
   } else if (btllib::SeedBloomFilter::is_bloom_file(bf_path)) {
+    timer.start("[-b] loading seed bloom filter from " + bf_path);
     btllib::SeedBloomFilter bf(bf_path);
-    btllib::SeedNtHash hash_fn("", bf.get_seeds(), bf.get_hash_num(), bf.get_k());
-    query<btllib::SeedBloomFilter, btllib::SeedNtHash>(bf, hash_fn, query_data, distinct);
+    timer.stop();
+    btllib::SeedNtHash hash_fn(
+      std::string(bf.get_k(), 'N'), bf.get_seeds(), bf.get_hash_num(), bf.get_k());
+    query<btllib::SeedBloomFilter, btllib::SeedNtHash>(bf, hash_fn, files, distinct, out_path);
   } else {
     std::cerr << "invalid bloom filter type (-b)" << std::endl;
     return EXIT_FAILURE;
