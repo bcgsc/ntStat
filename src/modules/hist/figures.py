484f241f422f5381cca7c272370efd6e3ef10f6c
parham-k <pkazemi3@gmail.com>
2024-07-11 23:43:26 -0700

    Update styles

--- src/modules/hist/figures.py
+++ src/modules/hist/figures.py
@@ -19,13 +19,14 @@ def plot(
 ):
     x_range = np.arange(x_min, x_max + 1)
     plt.style.use(style)
-    fig, ax = plt.subplots()
     colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+    fig, ax = plt.subplots()
     ax.set_yscale("log" if y_log else ax.get_yscale())
     ax.set_xlabel("K-mer count")
     ax.set_ylabel("Frequency")
 
-    bars = ax.bar(x_range, hist.values[x_range - 1], alpha=0.5)
+    bars = ax.bar(x_range, hist.values[x_range - 1])
+    plt.plot([], [])  # shift the color map
     y_err = err_rv.pdf(x_range) * hist.values.sum()
     ax.plot(x_range, y_err, label=f"Weak k-mers ({err_rv.dist.name})")
     y_gmm = np.zeros(x_range.shape[0])
@@ -33,7 +34,7 @@ def plot(
         y = w * rv.pdf(x_range) * gmm_norm
         y_gmm += y
         ax.plot(x_range, y, label=f"Component {i + 1} ({rv.dist.name})")
-    ax.plot(x_range, y_gmm, label="Mixture model", linestyle="dashed")
+    ax.plot(x_range, y_gmm, label="Mixture model", linestyle="--", linewidth=2.5)
 
     thresholds = {
         "First minima": hist.first_minima + 1,
@@ -44,9 +45,11 @@ def plot(
     handles, labels = ax.get_legend_handles_labels()
     for i, (name, x) in enumerate(thresholds.items()):
         if x_range[0] <= x <= x_range[-1]:
-            bars[x - 1].set_color(colors[i + 1])
-            handles.append(matplotlib.patches.Patch(facecolor=colors[i + 1]))
+            color = colors[(i + 5) % len(colors)]
+            bars[x - 1].set_color(color)
+            handles.append(matplotlib.patches.Patch(facecolor=color))
             labels.append(name)
 
-    ax.legend(handles=handles, labels=labels)
+    ax.set_ylim(bottom=1)
+    ax.legend(handles=handles, labels=labels, ncols=2)
     fig.savefig(out_path)
