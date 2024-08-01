9afd2e11d075c21d278272f1866b0dd2581d4527
parham-k <pkazemi3@gmail.com>
2024-08-01 13:34:25 -0700

    Update model fitting procedure

--- src/modules/hist/model.py
+++ src/modules/hist/model.py
@@ -61,22 +61,20 @@ class Model:
 
     def fit(self, hist: NtCardHistogram) -> int:
         num_iters = 0
-        x = np.arange(1, hist.max_count + 1)
-        self.__x_fit = x.copy()
-        y = hist.as_distribution()
+        self.__x_fit = np.arange(1, hist.max_count + 1)
+        x = np.arange(1, hist.first_minima + 1)
+        y = hist.as_distribution()[x - 1]
         err_rv = scipy.stats.burr
         p_err, _, info, *_ = scipy.optimize.curve_fit(
             err_rv.pdf, x, y, [0.5, 0.5, 0.5], full_output=True, maxfev=5000
         )
         self.__err_rv = err_rv(*p_err)
         num_iters += info["nfev"]
-        d = skimage.filters.threshold_multiotsu(hist=(y, x))[-1]
-        y = np.clip(hist.as_distribution() - self.__err_rv.pdf(x), 0, None)
-        b = ([0] * 8, [np.inf] * 8)
-        b[0][2], b[1][2] = hist.first_minima, 2 * d / 2 - hist.first_minima
-        b[0][6], b[1][6] = d / 2, 3 * d / 2
+        x = np.arange(hist.first_minima + 1, hist.max_count + 1)
+        y = np.clip(hist.as_distribution()[x - 1] - self.__err_rv.pdf(x), 0, None)
+        p0 = [0.5, 1, hist.otsu_thresholds[0], 1, 0.5, 1, hist.otsu_thresholds[1], 1]
         p, _, info, *_ = scipy.optimize.curve_fit(
-            gmm, x, y, bounds=b, method="trf", full_output=True, maxfev=Model.MAX_ITERS
+            gmm, x, y, full_output=True, p0=p0, maxfev=Model.MAX_ITERS
         )
         i = len(p) // 2
         self.__het = (p[0], scipy.stats.skewnorm(*p[1:i]))
