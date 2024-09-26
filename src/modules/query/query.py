f322986f5fc60e52262cebaeda7ce1a1e5141dba
parham-k <pkazemi3@gmail.com>
2024-09-26 10:51:10 -0700

    Fix query script

--- src/modules/query/query.py
+++ src/modules/query/query.py
@@ -20,28 +20,38 @@ def run(argv: list[str]):
         print("[-b] loading btllib::KmerBloomFilter... ", end="", flush=True)
         bf = btllib.KmerBloomFilter(args.b)
         hash_fn = btllib.NtHash
+        hash_fn_args = (bf.get_hash_num(), bf.get_k())
     elif btllib.KmerCountingBloomFilter8.is_bloom_file(args.b):
         print("[-b] loading btllib::KmerCountingBloomFilter8... ", end="", flush=True)
         bf = btllib.KmerCountingBloomFilter8(args.b)
         hash_fn = btllib.NtHash
+        hash_fn_args = (bf.get_hash_num(), bf.get_k())
     elif btllib.SeedBloomFilter.is_bloom_file(args.b):
         print("[-b] loading btllib::SeedBloomFilter... ", end="", flush=True)
         bf = btllib.SeedBloomFilter(args.b)
         hash_fn = btllib.SeedNtHash
+        hash_fn_args = (bf.get_seeds(), bf.get_hash_num(), bf.get_k())
     else:
         print("invalid bloom filter file", file=sys.stderr)
         return 1
     print("done")
-    print("initializing... ", end="", flush=True)
-    bf_all = btllib.BloomFilter(bf.get_bytes(), bf.get_hash_num())
+    all_kmers = set()
     out_file = open(args.o, "w")
     for file in args.data:
+        print(f"querying {file}... ", end="", flush=True)
         seq_reader = btllib.SeqReader(file, btllib.SeqReaderFlag.LONG_MODE)
         for record in seq_reader:
-            hash_iter = hash_fn(record.seq)
+            hash_iter = hash_fn(record.seq, *hash_fn_args)
             while hash_iter.roll():
-                if not bf_all.contains_insert(hash_iter.hashes()):
-                    kmer = record.seq[hash_iter.get_pos() : hash_iter.get_k()]
+                pos = hash_iter.get_pos()
+                kmer = record.seq[pos : pos + hash_iter.get_k()]
+                if kmer not in all_kmers:
+                    all_kmers.add(kmer)
                     value = bf.contains(hash_iter.hashes())
                     out_file.write(f"{kmer}\t{value}{os.linesep}")
+        print("done")
     return 0
+
+
+if __name__ == "__main__":
+    run(sys.argv)
