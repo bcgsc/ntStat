85a569215d12c8777a32b0e48a377c9e74f9a429
parham-k <pkazemi3@gmail.com>
2024-10-29 12:48:15 -0700

    Update installation test

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -4,6 +4,22 @@ import unittest
 
 class TestEntryPoint(unittest.TestCase):
 
-    def test_entry_point(self):
+    def test_main_entry_point(self):
         proc = subprocess.run(["ntstat", "--version"], stdout=subprocess.DEVNULL)
+        self.assertEqual(proc.returncode, 0)
+
+    def test_count_entry_point(self):
+        proc = subprocess.run(["ntstat", "count", "--help"], stdout=subprocess.DEVNULL)
+        self.assertEqual(proc.returncode, 0)
+    
+    def test_filter_entry_point(self):
+        proc = subprocess.run(["ntstat", "filter", "--help"], stdout=subprocess.DEVNULL)
+        self.assertEqual(proc.returncode, 0)
+
+    def test_hist_entry_point(self):
+        proc = subprocess.run(["ntstat", "hist", "--help"], stdout=subprocess.DEVNULL)
+        self.assertEqual(proc.returncode, 0)
+
+    def test_query_entry_point(self):
+        proc = subprocess.run(["ntstat", "query", "--help"], stdout=subprocess.DEVNULL)
         self.assertEqual(proc.returncode, 0)
